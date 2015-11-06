f1b14cc458fd09fc62a166792c51879efac1aa87
IntanRahmatillah <intanrahmatillah13@gmail.com>
2015-11-06 16:32:07 +0700

    komdat

--- 06-KopiBerkas/KopiBerkas.java
+++ 06-KopiBerkas/KopiBerkas.java
@@ -3,6 +3,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class KopiBerkas {
+    public static void main(String[] args) throws IOException {
+        try {
+            KopiBerkas k = new KopiBerkas();
+            k.kopi("intan.txt","baru.txt");
+        }
+        catch (IOException kesalahan) {
+            System.out.printf("Terjadi kesalahan: %s", kesalahan);
+        }
+    }
     public void kopi(String sumber, String sasaran) throws IOException {
         // Deklarasi variabel untuk stream
         FileInputStream masukan = null;
